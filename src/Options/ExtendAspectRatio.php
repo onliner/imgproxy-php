04f4f1faf82840caa06d133b3c9f2372e9fdfa8e
Alexander Mohorev <dev.mohorev@gmail.com>
2023-04-19 15:45:12 +0300

    Add `extend_aspect_ratio` processing option.

---
+++ src/Options/ExtendAspectRatio.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Onliner\ImgProxy\Options;
+
+final class ExtendAspectRatio extends Option
+{
+    /**
+     * @var Extend
+     */
+    private $extend;
+
+    public function __construct(bool $extend = true, string $gravity = null)
+    {
+        $this->extend = new Extend($extend, $gravity);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function name(): string
+    {
+        return 'exar';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function data(): array
+    {
+        return $this->extend->data();
+    }
+}
