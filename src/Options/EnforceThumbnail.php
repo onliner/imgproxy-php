11510eb68ec2ac1f852f8a8804bf61b695328721
Alexander Mohorev <dev.mohorev@gmail.com>
2023-04-19 15:04:15 +0300

    Add `enforce_thumbnail` processing option.

---
+++ src/Options/EnforceThumbnail.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Onliner\ImgProxy\Options;
+
+final class EnforceThumbnail extends Option
+{
+    /**
+     * @var string|null $format
+     */
+    private $format;
+
+    public function __construct(string $format = null)
+    {
+        $this->format = $format;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function name(): string
+    {
+        return 'eth';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function data(): array
+    {
+        return [
+            $this->format ?: true,
+        ];
+    }
+}
