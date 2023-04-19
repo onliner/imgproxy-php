f4ca449539961d636233fcc5f5ccc40852a0e547
Alexander Mohorev <dev.mohorev@gmail.com>
2023-04-19 14:35:21 +0300

    Add `zoom` processing option.

---
+++ src/Options/Zoom.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Onliner\ImgProxy\Options;
+
+use InvalidArgumentException;
+
+class Zoom extends Option
+{
+    /**
+     * @var float
+     */
+    private $x;
+    /**
+     * @var float|null
+     */
+    private $y;
+
+    public function __construct(float $x, float $y = null)
+    {
+        if ($x <= 0) {
+            throw new InvalidArgumentException(sprintf('Invalid zoom X value: %s', $x));
+        }
+
+        if ($y !== null && $y <= 0) {
+            throw new InvalidArgumentException(sprintf('Invalid zoom Y value: %s', $y));
+        }
+
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function name(): string
+    {
+        return 'z';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function data(): array
+    {
+        return [
+            $this->x,
+            $this->y,
+        ];
+    }
+}
